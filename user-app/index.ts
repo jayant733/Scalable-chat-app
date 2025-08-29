aacef9cd8a5d830bca308deaea54504936eac26f
Jayant Sharma <jayant.sharma2023@vitstudent.ac.in>
2025-08-29 23:04:20 +0530

    added first test

--- user-app/index.ts
+++ user-app/index.ts
@@ -1,15 +1,36 @@
-import { WebSocketServer } from 'ws';
+import { WebSocketServer , WebSocket } from 'ws';
 
 const wss = new WebSocketServer({ port: 8080 });
 
+interface room {
+    Socket  : WebSocket[] //create a websockcer array which contatin the interface array 
+
+}
+const rooms : Record<string, room> = {
+
+}
 wss.on('connection', (ws) => {
 	console.log('New client connected');
 
-	ws.on('message', (message) => {
-		console.log(`Received message: ${message}`);
-		// Echo the message back
-		ws.send(`Server received: ${message}`);
-	});
+	ws.on('message', function message(data : string) {
+            const parsedData = JSON.parse(data);
+            if(parsedData.type == "join-room"){
+                const room = parsedData.room;
+                if(!rooms[room]){
+                    rooms[room] ={
+                        Socket : []
+                    }
+                }
+
+                rooms[room].Socket.push(ws);
+            }
+
+            if(parsedData.type == "chat"){
+                const room = parsedData.room;
+                rooms[room]?.Socket.map(socket => socket.send(data))
+            }
+
+    });
 
 	ws.on('close', () => {
 		console.log('Client disconnected');
@@ -18,6 +39,9 @@ wss.on('connection', (ws) => {
 	ws.on('error', (err) => {
 		console.error('WebSocket error:', err);
 	});
+
+    ws.send("something");
 });
 
 console.log('WebSocket server is running on ws://localhost:8080');
+
